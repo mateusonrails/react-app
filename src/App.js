488870dfcd14ac610c2982b17f9ede655a5d17fa
Mateus Campos <mateus.camphos2000@gmail.com>
2025-02-25 23:26:41 -0300

    Add categories in application.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+
+
+const categories = [
+  "Anime",
+  "Filme",
+  "Série",
+  "Documentário"
+]
+
+function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id] )
+}
+
 
 function App() {
   return (
@@ -11,12 +25,23 @@ function App() {
       <Header/>
       <Banner image="home" />
       <Container>
-          <h2>ANIMES</h2>
-        <section className="cards">
-          { videos.map((video) => <Card id={video.id} key={video.id}/> ) }
-        </section>
+        <Category category="Anime">
+          { filterCategory(0).map((video) => <Card id={video.id} key={video.id}/> ) }
+        </Category>
 
+        <Category category="Filme">
+          { filterCategory(1).map((video) => <Card id={video.id} key={video.id}/> ) }
+        </Category>
 
+        <Category category="Série">
+          { filterCategory(2).map((video) => <Card id={video.id} key={video.id}/> ) }
+        </Category>
+
+        <Category category="Documentário">
+          { filterCategory(3).map((video) => <Card id={video.id} key={video.id}/> ) }
+        </Category>
+
+        
       </Container>
       <Footer/>
     </>
