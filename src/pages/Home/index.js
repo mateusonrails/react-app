3356ff4d8b2d57e9d21f81236c53a86e0a4f1abc
Mateus Campos <mateus.camphos2000@gmail.com>
2025-04-21 21:53:58 -0300

    Starting knowledge in hook with UseState.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,38 @@
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
-import Category, { categories, filterCategory } from "../../components/Category";
+// import Category, { categories, filterCategory } from "../../components/Category";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { useState } from "react";
 
 function Home() {
+
+  function getName(event) {
+    console.log(event.target.value)
+    setNome(event.target.value)
+  }
+
+  const [ name, setNome] = useState()
+
   return (
     <>
       <Header/>
       <Banner image="home" />
       <Container>
-        { categories.map((category, index) =>
+
+        <input 
+          type="text"
+          placeholder="name"
+          onChange={getName}
+        />
+        <h2>{name}</h2>
+
+        {/* { categories.map((category, index) =>
           <Category category={category}>
             { filterCategory(index).map((video) => <Card video={video} key={video.key}/> ) }
           </Category>
-        )}
+        )} */}
       </Container>
       <Footer/>
     </>
